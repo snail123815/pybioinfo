915c2b5a46925bf37bc2ee2bac4a4b94a3625a95
Chao Du <snail123815@users.noreply.github.com>
2024-10-10 09:43:09 +0200

    Refactor run time messages and improve code formatting

--- chipSeq_processSam2Bam.py
+++ chipSeq_processSam2Bam.py
@@ -4,9 +4,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--pathSam', help='path to sam files')
-parser.add_argument('-o', '--output', help='path to sorted bam files')
-parser.add_argument('-t', '--threads', help='number of threads', type=int)
+parser.add_argument("-p", "--pathSam", help="path to sam files")
+parser.add_argument("-o", "--output", help="path to sorted bam files")
+parser.add_argument("-t", "--threads", help="number of threads", type=int)
 
 args = parser.parse_args()
 
@@ -20,30 +20,42 @@ if not os.path.isdir(pathOut):
 fileList = []
 for path, subdirs, files in os.walk(pathSam):
     for f in files:
-        if f.endswith('.sam'):
+        if f.endswith(".sam"):
             filePath = os.path.join(path, f)
             fileList.append(filePath)
 
 for f in fileList:
     name = os.path.split(f)[1]
-    bam = os.path.join(pathOut, f'{name[:-4]}.bam')
-    bai = os.path.join(pathOut, f'{name[:-4]}.bai')
+    bam = os.path.join(pathOut, f"{name[:-4]}.bam")
+    bai = os.path.join(pathOut, f"{name[:-4]}.bai")
 
-    cmd = ['samtools', 'sort',
-            '-m', '20G',
-            '-o', bam,
-            '-O', 'bam',
-            '-@', ncpu,
-            f]
-    print('Running...\n', ' '.join(cmd))
+    cmd = [
+        "samtools",
+        "sort",
+        "-m", "20G",
+        "-o", bam,
+        "-O",
+        "bam",
+        "-@", ncpu,
+        f,
+    ]
+    print("Running...\n", " ".join(cmd))
     p = subprocess.run(cmd, capture_output=True)
-    print('output:\n', p.stdout.decode('utf-8'), '\n')
-    print('error:\n', p.stderr.decode('utf-8'), '\n')
+    stdout = p.stdout.decode("utf-8")
+    stderr = p.stderr.decode("utf-8")
+    if len(stdout) > 0:
+        print("stdout:\n", stdout)
+    if len(stderr) > 0:
+        print("stderr:\n", stderr)
+    print("output file: ", bam, "\n")
 
-    cmd = ['samtools', 'index',
-            bam, bai]
-    print('Running...\n', ' '.join(cmd))
-    p = subprocess.run(cmd, capture_output=True)
-    print('output:\n', p.stdout.decode('utf-8'), '\n')
-    print('error:\n', p.stderr.decode('utf-8'), '\n')
-    
+    cmd = ["samtools", "index", bam, bai]
+    print("Running...\n", " ".join(cmd))
+    stdout = p.stdout.decode("utf-8")
+    stderr = p.stderr.decode("utf-8")
+    if len(stdout) > 0:
+        print("stdout:\n", stdout)
+    if len(stderr) > 0:
+        print("stderr:\n", stderr)
+    print("\n")
+    print("output file: ", bai, "\n")
