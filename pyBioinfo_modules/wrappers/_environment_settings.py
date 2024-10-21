9b2d31ca5e31735ef22ba2cee5b22dfacdbb6d56
Chao Du <snail123815@users.noreply.github.com>
2024-10-21 11:43:30 +0200

    Mearge main

--- pyBioinfo_modules/wrappers/_environment_settings.py
+++ pyBioinfo_modules/wrappers/_environment_settings.py
@@ -14,8 +14,9 @@ PROKKA_ENV: Path | None = Path.home() / "genvs/quasan"
 MASH_ENV: Path | None = Path.home() / "genvs/phylophlan"
 BIGSCAPE_ENV: Path | None = Path.home() / "genvs/bigscape"
 SHORTREADS_ENV: Path | None = Path.home() / "genvs/shortReads"
-
+HMMER_ENV: Path | None = Path.home() / "genvs/antismash"
 PFAM_DB: Path | None = None
+
 try:
     PFAM_DB = sorted(
         [
