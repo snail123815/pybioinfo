cabb521d67067638791eada944352f3dfbe2aba3
Chao Du <snail123815@users.noreply.github.com>
2024-11-04 23:12:09 +0100

    Refactor MACS pileup reading and plotting functions; update genomic region in launch configuration

--- pyBioinfo_modules/bio_sequences/plot_genes.py
+++ pyBioinfo_modules/bio_sequences/plot_genes.py
@@ -1,27 +1,51 @@
-import matplotlib.transforms as mtransforms
-from matplotlib.patches import FancyArrow, Rectangle
-from matplotlib.axes import Axes
-from Bio.SeqRecord import SeqRecord
+import logging
 from typing import List, Literal
+
+import matplotlib.transforms as mtransforms
+from Bio.SeqRecord import SeqRecord
+from matplotlib.axes import Axes
 from matplotlib.lines import Line2D
+from matplotlib.patches import FancyArrow, Rectangle
 from matplotlib.text import Text
+
+logger = logging.getLogger(__name__)
 
 
 def make_gene_appear_truncated(
     ax: Axes,
     truncate_loc: float,
     y_loc: float,
-    direction: Literal[-1, 1],
+    block_direction: Literal[-1, 1],
     block_width: float,
     block_length: float,
     num_segments=2,
     transform=None,
     zorder=None,
 ) -> List[Rectangle]:
+    """
+    Create a visual representation of a truncated gene on a plot by adding
+    rectangular blocks at the truncated end.
+
+    Parameters:
+    ax (Axes): The matplotlib Axes object to draw the blocks on.
+    truncate_loc (float): The location on the x-axis where the truncation starts.
+    y_loc (float): The location on the y-axis where the blocks will be centered.
+    direction (Literal[-1, 1]): The direction in which the blocks will be drawn.
+                                Use -1 for left and 1 for right.
+    block_width (float): The width of each block.
+    block_length (float): The length of each block.
+    num_segments (int, optional): The number of blocks to draw. Default is 2.
+    transform (optional): The transformation applied to the blocks. Default is None.
+    zorder (optional): The z-order of the blocks. Default is None.
+
+    Returns:
+    List[Rectangle]: A list of matplotlib Rectangle objects representing
+                     the blocks.
+    """
     blocks = []
     for i in range(num_segments):
-        segment_start = truncate_loc + direction * block_length * (
-            2 * i + (2 if direction == -1 else 1)
+        segment_start = truncate_loc + block_direction * block_length * (
+            2 * i + (2 if block_direction == -1 else 1)
         )
         block = Rectangle(
             (segment_start, y_loc - block_width / 2),
@@ -46,7 +70,33 @@ def plot_genes(
     region_end: int,
     color: List[str] = ["C0", "C2"],
     max_genes: int = 10,
-) -> tuple[List[FancyArrow], dict[str:Rectangle], Line2D, list[Text]]:
+) -> tuple[List[FancyArrow], dict[str:[Rectangle]], Line2D, list[Text]]:
+    """
+    Plots genes on a given matplotlib Axes object within a specified
+    genomic region.
+
+    Parameters:
+    ax (Axes): Matplotlib Axes object where the genes will be plotted.
+    genome_with_annotation (SeqRecord): A SeqRecord object containing the genome
+                                        sequence and its annotations.
+    region_start (int): The start position of the genomic region to be plotted.
+    region_end (int): The end position of the genomic region to be plotted.
+    color (List[str], optional): List of colors for the genes on forward and
+                                 reverse strand, respectively.
+                                 Default is ["C0", "C2"].
+    max_genes (int, optional): Maximum number of gene names to display.
+                               Default is 10.
+
+    Returns:
+    tuple: A tuple containing:
+        - List[FancyArrow]: List of FancyArrow objects representing the genes.
+        - dict[str:list[Rectangle]]: Dictionary with keys "left" and "right"
+                               containing Rectangle objects for truncated genes.
+        - Line2D: Line2D object representing the genome sequence blackline.
+        - list[Text]: List of Text objects (axes.Text) for gene names. Including
+                      the gene names that are not displayed if max_genes is
+                      exceeded.
+    """
     xrange = ax.get_xlim()[1] - ax.get_xlim()[0]
     arrow_y_loc = 0.1
     # Gene x-axis position will be defined by gene location itself,
@@ -65,29 +115,35 @@ def plot_genes(
     arrows = []
     truncate_blocks = {"left": [], "right": []}
 
+    gene_nr_in_region = 0
+    center_gene_nr = None
     for feature in genome_with_annotation.features:
         if feature.type == "gene":
             gene_start = feature.location.start
             gene_end = feature.location.end
             gene_strand = feature.location.strand
 
-            partial_right = False
-            partial_left = False
             if gene_end < region_start:
                 continue
             if gene_start > region_end:
                 break
+
+            if gene_start < (region_start / 2 + region_end / 2) < gene_end:
+                center_gene_nr = gene_nr_in_region
+            gene_nr_in_region += 1
+            partial = None
             if gene_start < region_start:
                 gene_start = region_start
-                partial_left = True
+                partial = "left"
             if gene_end > region_end - 1:
                 gene_end = region_end - 1
-                partial_right = True
+                partial = "right"
 
             arrow_color = color[0] if gene_strand == 1 else color[1]
 
             # Define fixed head width and head length
             head_width = 0.05
+            # Head length is 0.7% of the x-axis range, reduce if gene is shorter
             head_length = min(abs(gene_end - gene_start), 0.007 * xrange)
 
             # Add arrowhead
@@ -111,12 +167,15 @@ def plot_genes(
             arrows.append(arrow)
             ax.add_patch(arrow)
 
-            if partial_left:
-                truncate_blocks["left"] = make_gene_appear_truncated(
+            # Add truncated blocks
+            if partial:
+                trunc_loc = gene_start if partial == "left" else gene_end
+                block_direction = 1 if partial == "left" else -1
+                truncate_blocks[partial] = make_gene_appear_truncated(
                     ax=ax,
-                    truncate_loc=gene_start,
+                    truncate_loc=trunc_loc,
                     y_loc=arrow_y_loc,
-                    direction=1,
+                    block_direction=block_direction,
                     block_width=head_width * 1.02,
                     # A multiplier > 1 to make the block just a bit wider than
                     # the arrow head, preventing line leakage when rendered.
@@ -124,27 +183,17 @@ def plot_genes(
                     transform=trans,
                     zorder=1,
                 )
+                # Reduce the genome_line from left by an arrow head length
                 if gene_strand == -1:
-                    # Reduce the genome_line from left by an arrow head length
                     genome_line.set_xdata(
                         [region_start + head_length, region_end]
                     )
-            if partial_right:
-                truncate_blocks["right"] = make_gene_appear_truncated(
-                    ax=ax,
-                    truncate_loc=gene_end,
-                    y_loc=arrow_y_loc,
-                    direction=-1,
-                    block_width=head_width * 1.02,
-                    block_length=head_length * 0.4,
-                    transform=trans,
-                    zorder=1,
-                )
-                if gene_strand == 1:
-                    # Reduce the genome_line from right by an arrow head length
+                else:
                     genome_line.set_xdata(
                         [region_start, region_end - head_length]
                     )
+
+            # Add gene name text
             gene_names.append(
                 ax.text(
                     (gene_start + gene_end) / 2,
@@ -157,9 +206,9 @@ def plot_genes(
                 )
             )
 
-    # Reduce the length of the genome line by 3 times the pixel length
-    # to prevent the line from touching the end of the plotted gene at render,
-    # especially when the gene is at the edge of the plot.
+    # Reduce the length of the genome line by 3 times the pixel length to
+    # prevent the line from touching the end of the plotted gene at render,
+    # especially when the gene is at the edge of the plot. Leakage is bad.
     pixel_length = (region_end - region_start) / ax.get_window_extent().width
     genome_line.set_xdata(
         [
@@ -168,11 +217,23 @@ def plot_genes(
         ]
     )
 
+    # Hide gene names if there are too many,
+    # only show 10 genes at an average interval
     if len(gene_names) >= max_genes:
-        interval = len(gene_names) // 10
-        for i, gene_name in enumerate(gene_names):
-            if i % interval != 0:
-                gene_name.set_visible(False)
+        interval = len(gene_names) // max_genes
+        if center_gene_nr:
+            for i, gene_name in enumerate(gene_names[center_gene_nr:]):
+                if i % interval != 0:
+                    gene_name.set_visible(False)
+            for i, gene_name in enumerate(
+                reversed(gene_names[: center_gene_nr])
+            ):
+                if (interval + i + 1) % interval != 0:
+                    gene_name.set_visible(False)
+        else:
+            for i, gene_name in enumerate(gene_names):
+                if (interval//2 + i + 1) % interval != 0:
+                    gene_name.set_visible(False)
 
     return (
         arrows,
--- pyBioinfo_modules/chipseq/coverage.py
+++ pyBioinfo_modules/chipseq/coverage.py
@@ -1,10 +1,13 @@
-from pathlib import Path
-import pandas as pd
 import bz2
-from pandas import DataFrame
 import logging
+import lzma
 import pickle
+from pathlib import Path
 from typing import TextIO
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
 
 
 class CoverageData():
@@ -55,7 +58,7 @@ class CoverageData():
         return zipFile
 
 
-def read_macs_pileup(
+def _read_one_macs_pileup(
     pileup: TextIO, tr_start: int, tr_end: int
 ) -> list[tuple[int, float]]:
     # Read the pileup file, a range per line
@@ -77,3 +80,87 @@ def read_macs_pileup(
         for i in effective_range:
             tr_perbase_pileup.append((i, value))
     return tr_perbase_pileup
+
+def read_macs_pileup(
+    macsOutputPath: Path, tr_start: int, tr_end: int
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Reads and processes the input arguments for the chipSeq plot pileup
+    comparisons.
+
+    Args:
+        macsOutputPath (Path): The path to the macs output dir.
+            
+        tr_start (int): The start position of the genomic region to plot.
+        tr_end (int): The end position of the genomic region to plot.
+
+    Returns:
+        tuple: A tuple containing:
+        The control data array with genomic positions and pileup values.
+            - tr_control_data (np.ndarray): The control data
+            - tr_treat_data (np.ndarray): The treatment data
+
+    Raises:
+        ValueError: If the region format is invalid or the gene is not found
+        in the genome file.
+
+    The function performs the following steps:
+    1. Reads the genome file with annotations.
+    2. If a region is specified, it parses the region and extracts the start
+       and end positions.
+    3. If a gene is specified, it finds the gene in the genome annotations and
+       calculates the start and end positions based on the flanking region.
+    """
+    # Find all matching files
+    pileup_files = [
+        file
+        for file in macsOutputPath.expanduser().glob("*.*")
+        if file.name.endswith(".bdg") or file.name.endswith(".bdg.xz")
+    ]
+
+    assert len(pileup_files) == 2, (
+        "There should be two and only two .bdg files in the macs output dir."
+        f" Found {len(pileup_files)} files." + str(pileup_files)
+    )
+
+    control_lambda_path = None
+    treat_pileup_path = None
+
+    for f in pileup_files:
+        if "_control_lambda" in f.name:
+            control_lambda_path = f
+        elif "_treat_pileup" in f.name:
+            treat_pileup_path = f
+        else:
+            raise ValueError(
+                "Unknown file: "
+                + f
+                + ". Expected either _control_lambda or _treat_pileup"
+            )
+
+    assert control_lambda_path, (
+        "Control pileup file not found. "
+        "Please make sure the file name contains '_control_lambda'."
+    )
+    assert treat_pileup_path, (
+        "Experimental pileup file not found. "
+        "Please make sure the file name contains '_treat_pileup'."
+    )
+
+    if control_lambda_path.suffix == ".xz":
+        control_lambda = lzma.open(control_lambda_path, "rt")
+    else:
+        control_lambda = open(control_lambda_path, "r")
+    if treat_pileup_path.suffix == ".xz":
+        treat_pileup = lzma.open(treat_pileup_path, "rt")
+    else:
+        treat_pileup = open(treat_pileup_path, "r")
+
+    tr_control_data = np.array(
+         _read_one_macs_pileup(control_lambda, tr_start, tr_end)
+    )
+    tr_treat_data = np.array( _read_one_macs_pileup(treat_pileup, tr_start, tr_end))
+    control_lambda.close()
+    treat_pileup.close()
+
+    return tr_control_data, tr_treat_data
