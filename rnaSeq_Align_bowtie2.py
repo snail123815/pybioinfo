ef713d10b34eac4539158cc6637c9c9efd300874
Chao Du <snail123815@users.noreply.github.com>
2024-11-19 22:45:31 +0100

    extract get_read_files_per_sample() from main()

--- rnaSeq_Align_bowtie2.py
+++ rnaSeq_Align_bowtie2.py
@@ -12,6 +12,7 @@ from pyBioinfo_modules.wrappers.bowtie2 import buildBowtie2idx, runBowtie2
 
 logger = logging.getLogger(__name__)
 
+
 def arg_setup():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -58,43 +59,85 @@ def arg_setup():
     return parser
 
 
-def main(args):
+def get_read_files_per_sample(
+    parent_dir: list[Path], sample_names: list[str] | None = None, isPe: bool = False
+) -> tuple[dict[str, list[Path]], list[str], str]:
     # Gether all files, max depth 2
-    filePaths: list[Path] = []
-    for d in args.raw:
+    file_paths: list[Path] = []
+    for d in parent_dir:
         for n in d.iterdir():
             if n.is_file():
-                filePaths.append(n)
+                file_paths.append(n)
             elif n.is_dir():
-                filePaths.extend(f for f in n.iterdir() if f.is_file())
-    for f in filePaths.copy():
+                file_paths.extend(f for f in n.iterdir() if f.is_file())
+    for f in file_paths.copy():
         ext = splitStemSuffixIfCompressed(f)[1]
         if len(ext.split(".")) < 2 or ext.split(".")[1] not in ["fastq", "fq"]:
-            filePaths.remove(f)
-    assert len(filePaths) > 0, f"Files not found in {args.raw}"
-    FILE_PATHS = sorted(filePaths)
-    FILE_PARTS = [
-        splitStemSuffixIfCompressed(f, fullSuffix=True) for f in FILE_PATHS
+            file_paths.remove(f)
+    assert len(file_paths) > 0, f"Files not found in {args.raw}"
+    file_paths = sorted(file_paths)
+    file_parts = [
+        splitStemSuffixIfCompressed(f, fullSuffix=True) for f in file_paths
     ]
-    FILE_NAMES = [fp[0] for fp in FILE_PARTS]
-    FILE_FULLEXTS = [fp[1] for fp in FILE_PARTS]
-    assert len(set(FILE_FULLEXTS)) == 1, (
+    file_names = [fp[0] for fp in file_parts]
+    file_fullexts = [fp[1] for fp in file_parts]
+    assert len(set(file_fullexts)) == 1, (
         "All files should have same format, "
-        f"yet multiple found {set(FILE_FULLEXTS)}."
+        f"yet multiple found {set(file_fullexts)}."
     )
-    assert len(set(FILE_NAMES)) == len(FILE_NAMES), (
+    assert len(set(file_names)) == len(file_names), (
         "File names has duplicates"
         ", maybe from different dirs?\n"
-        f"{set([fn for fn in FILE_NAMES if FILE_NAMES.count(fn) > 1])}"
+        f"{set([fn for fn in file_names if file_names.count(fn) > 1])}"
     )
 
-    if not args.out.is_dir():
-        args.out.mkdir(exist_ok=True)
-    
+    if sample_names is not None:
+        samples = sample_names
+    else:
+        samples = []
+
+    sample_file_dict: dict[str, list[Path]] = {}
+    peSfx: list[str] = []
+    if isPe:
+        peSfx = imputePeSuffix(file_names)
+        if samples == []:
+            samples = (
+                sorted(list(set(f[: -len(peSfx[0])] for f in file_names)))
+                if len(samples) == 0
+                else samples
+            )
+            assert (len(file_paths) / len(samples)) % 2 == 0
+        for s in samples:
+            fns = [file_paths[i] for i, fn in enumerate(file_names) if s in fn]
+            assert len(fns) > 0, f"Did not find files for sample {s}"
+            assert len(fns) % 2 == 0, (
+                f"Number of files for sample {s} is not paired.\n" f"{fns}"
+            )
+            sample_file_dict[s] = fns
+    else:  # single end reads
+        if samples == []:
+            for fn, fp in zip(file_names, file_paths):
+                sample_file_dict[fn] = [fp]
+            samples = file_names
+        elif len(samples) == 1:
+            sample_file_dict[samples[0]] = file_paths
+        else:
+            for s in samples:
+                fps = [
+                    file_paths[i] for i, fn in enumerate(file_names) if s in fn
+                ]
+                assert len(fps) > 0, f"Did not find files for sample {s}"
+                sample_file_dict[s] = fps
+
+    return sample_file_dict, peSfx, file_fullexts[0]
+
+
+def main(args):
+    # Gether all files, max depth 2
     # Process loggers
     log_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     log_file = args.out / "align.log"
     log_file_handler = logging.FileHandler(log_file)
     log_file_handler.setFormatter(log_formatter)
@@ -110,52 +153,20 @@ def main(args):
     if args.dryRun:
         logger.debug("=" * 20 + "Dry run, will not execute bowtie2")
 
-    if args.sampleNames is not None:
-        samples = args.sampleNames
-    else:
-        samples = []
-
-    sampleFileDict: dict[str, list[Path]] = {}
-
-    peSfx: list[str] = []
-    if args.isPe:
-        peSfx = imputePeSuffix(FILE_NAMES)
-        if samples == []:
-            samples = (
-                sorted(list(set(f[: -len(peSfx[0])] for f in FILE_NAMES)))
-                if len(samples) == 0
-                else samples
-            )
-            assert (len(FILE_PATHS) / len(samples)) % 2 == 0
-        for s in samples:
-            fns = [FILE_PATHS[i] for i, fn in enumerate(FILE_NAMES) if s in fn]
-            assert len(fns) > 0, f"Did not find files for sample {s}"
-            assert len(fns) % 2 == 0, (
-                f"Number of files for sample {s} is not paired.\n" f"{fns}"
-            )
-            sampleFileDict[s] = fns
-
-    else:  # single end reads
-        if samples == []:
-            for fn, fp in zip(FILE_NAMES, FILE_PATHS):
-                sampleFileDict[fn] = [fp]
-            samples = FILE_NAMES
-        elif len(samples) == 1:
-            sampleFileDict[samples[0]] = FILE_PATHS
-        else:
-            for s in samples:
-                fps = [
-                    FILE_PATHS[i] for i, fn in enumerate(FILE_NAMES) if s in fn
-                ]
-                assert len(fps) > 0, f"Did not find files for sample {s}"
-                sampleFileDict[s] = fps
-
     tInit = time.time()
+
+    sample_file_dict, peSfx, file_fullext = get_read_files_per_sample(
+        args.raw, args.sampleNames, args.isPe
+    )
+    samples = sorted(list(sample_file_dict.keys()))
+    logger.info(f"Samples to process: {samples}")
+
+    if not args.out.is_dir():
+        args.out.mkdir(exist_ok=True)
 
     genomeBowtie2Idx = buildBowtie2idx(args.genome, out=args.out / "genomeIdx")
 
-    logger.info(f"Samples to process: {samples}")
-    for i, (s, fps) in enumerate(sampleFileDict.items()):
+    for i, (s, fps) in enumerate(sample_file_dict.items()):
         logger.info(f"Processing {i+1}/{len(samples)}: {s}")
 
         # prepare align arguments
@@ -164,7 +175,7 @@ def main(args):
             samples1: list[Path] = []
             samples2: list[Path] = []
             for fp in fps:
-                suffex_len_with_pe: int = len(FILE_FULLEXTS[0]) + len(peSfx[0])
+                suffex_len_with_pe: int = len(file_fullext) + len(peSfx[0])
                 for j in range(suffex_len_with_pe, len(fp.name)):
                     if any(sfx in fp.name[-j:] for sfx in peSfx):
                         suffex_len_with_pe = j
@@ -198,7 +209,6 @@ def main(args):
     logger.info(f"All done, time elapsed {timeDiffStr(tInit)}")
     logger.info("=" * 20 + getTimeStr() + "=" * 20 + "\n" * 2)
     logger.handlers.clear()
-
 
 
 def imputePeSuffix(rawFileNames: list[str], peSfx: list[str] = []):
