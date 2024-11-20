baa7c9918f9ecdee0c0d20a76809780ac29e2118
Chao Du <snail123815@users.noreply.github.com>
2024-11-20 10:29:22 +0100

    Refactor featureCounts logic into a dedicated function for improved readability and maintainability

--- pyBioinfo_modules/wrappers/featureCounts.py
+++ pyBioinfo_modules/wrappers/featureCounts.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import subprocess
 import time
 from pathlib import Path
@@ -12,9 +11,106 @@ from Bio.SeqRecord import SeqRecord
 
 from pyBioinfo_modules.basic.basic import getTimeStr, timeDiffStr
 from pyBioinfo_modules.wrappers._environment_settings import (
-    SHELL, SHORTREADS_ENV, withActivateEnvCmd)
+    SHELL,
+    SHORTREADS_ENV,
+    withActivateEnvCmd,
+)
 
 logger = logging.getLogger(__name__)
+
+
+def featureCounts(
+    file: Path,
+    gffFile: Path,
+    ncpu: int,
+    output_path: Path,
+    isPe: bool,
+    targetFeature: str,
+    groupFactor: str,
+    fractionCounting: bool,
+    peLoose: bool,
+    dryRun: bool = False,
+):
+    ts = time.time()
+    b = file.stem
+    cmdList = [
+        "featureCounts",
+        "-T",
+        str(ncpu),
+        "-a",
+        str(gffFile),
+        "-F",
+        "GTF",
+        "-t",
+        targetFeature,
+        "-g",
+        groupFactor,
+        "--minOverlap",
+        "20",  # Minimum number of overlapping bases in a read that is
+        # required for read assignment. 1 by default. Number of
+        # overlapping bases is counted from both reads if paired
+        # end. If a negative value is provided, then a gap of up
+        # to specified size will be allowed between read and the
+        # feature that the read is assigned to.
+        "--fracOverlap",
+        "0.25",  # Minimum fraction of overlapping bases in a read that is
+        # required for read assignment. Value should be within range
+        # [0,1]. 0 by default. Number of overlapping bases is
+        # counted from both reads if paired end. Both this option
+        # and '--minOverlap' option need to be satisfied for read
+        # assignment.
+        "-o",
+        str(output_path / f"{b}.txt"),
+        str(file),
+    ]
+    if fractionCounting:
+        # Multi-mapping reads will also be counted. For a multi-
+        cmdList.insert(3, "-M")
+        # mapping read, all its reported alignments will be
+        # counted. The 'NH' tag in BAM/SAM input is used to detect
+        # multi-mapping reads.
+        # Assign fractional counts to features. This option must
+        cmdList.insert(4, "--fraction")
+        # be used together with '-M' or '-O' or both. When '-M' is
+        # specified, each reported alignment from a multi-mapping
+        # read (identified via 'NH' tag) will carry a fractional
+        # count of 1/x, instead of 1 (one), where x is the total
+        # number of alignments reported for the same read. When '-O'
+        # is specified, each overlapping feature will receive a
+        # fractional count of 1/y, where y is the total number of
+        # features overlapping with the read. When both '-M' and
+        # '-O' are specified, each alignment will carry a fractional
+        # count of 1/(x*y).
+        # Assign reads to all their overlapping meta-features (or
+        cmdList.insert(5, "-O")
+        # features if -f is specified).
+
+    if isPe:
+        # If specified, fragments (or templates) will be counted
+        cmdList.insert(3, "-p")
+        # instead of reads. This option is only applicable for
+        # paired-end reads; single-end reads are always counted as
+        # reads.
+        if not peLoose:
+            # Check validity of paired-end distance when counting read
+            cmdList.insert(4, "-P")
+            # pairs. Use -d and -D to set thresholds.
+            cmdList.insert(5, "-B")  # Only count read pairs that have both ends
+            # aligned. (must set together with -P)
+    logger.info(" ".join(cmdList))
+    cmd = withActivateEnvCmd(" ".join(cmdList), SHORTREADS_ENV)
+    if dryRun:
+        logger.info("Dry run, not executing command")
+        logger.info(cmd)
+        return
+    res = subprocess.run(cmd, shell=True, capture_output=True, executable=SHELL)
+    if res.returncode != 0:
+        logger.info(res.stdout.decode())
+        logger.info(res.stderr.decode())
+        raise Exception
+    logger.info(res.stderr.decode())
+    logger.info(f"Finished in {timeDiffStr(ts)}\n")
+
 
 def multiple_featureCounts(
     input_path: Path,
@@ -101,89 +197,18 @@ def multiple_featureCounts(
 
     for i, f in enumerate(files):
         logger.info(f"Processing {i+1}/{len(files)}: ")
-        ts = time.time()
-        b = os.path.splitext(os.path.split(f)[-1])[0]
-        cmdList = [
-            "featureCounts",
-            "-T",
-            str(ncpu),
-            "-a",
-            str(gffFile),
-            "-F",
-            "GTF",
-            "-t",
-            targetFeature,
-            "-g",
-            groupFactor,
-            "--minOverlap",
-            "20",  # Minimum number of overlapping bases in a read that is
-            # required for read assignment. 1 by default. Number of
-            # overlapping bases is counted from both reads if paired
-            # end. If a negative value is provided, then a gap of up
-            # to specified size will be allowed between read and the
-            # feature that the read is assigned to.
-            "--fracOverlap",
-            "0.25",  # Minimum fraction of overlapping bases in a read that is
-            # required for read assignment. Value should be within range
-            # [0,1]. 0 by default. Number of overlapping bases is
-            # counted from both reads if paired end. Both this option
-            # and '--minOverlap' option need to be satisfied for read
-            # assignment.
-            "-o",
-            str(output_path / f"{b}.txt"),
-            str(f),
-        ]
-        if fractionCounting:
-            # Multi-mapping reads will also be counted. For a multi-
-            cmdList.insert(3, "-M")
-            # mapping read, all its reported alignments will be
-            # counted. The 'NH' tag in BAM/SAM input is used to detect
-            # multi-mapping reads.
-            # Assign fractional counts to features. This option must
-            cmdList.insert(4, "--fraction")
-            # be used together with '-M' or '-O' or both. When '-M' is
-            # specified, each reported alignment from a multi-mapping
-            # read (identified via 'NH' tag) will carry a fractional
-            # count of 1/x, instead of 1 (one), where x is the total
-            # number of alignments reported for the same read. When '-O'
-            # is specified, each overlapping feature will receive a
-            # fractional count of 1/y, where y is the total number of
-            # features overlapping with the read. When both '-M' and
-            # '-O' are specified, each alignment will carry a fractional
-            # count of 1/(x*y).
-            # Assign reads to all their overlapping meta-features (or
-            cmdList.insert(5, "-O")
-            # features if -f is specified).
-
-        if isPe:
-            # If specified, fragments (or templates) will be counted
-            cmdList.insert(3, "-p")
-            # instead of reads. This option is only applicable for
-            # paired-end reads; single-end reads are always counted as
-            # reads.
-            if not peLoose:
-                # Check validity of paired-end distance when counting read
-                cmdList.insert(4, "-P")
-                # pairs. Use -d and -D to set thresholds.
-                cmdList.insert(
-                    5, "-B"
-                )  # Only count read pairs that have both ends
-                # aligned. (must set together with -P)
-        logger.info(" ".join(cmdList))
-        cmd = withActivateEnvCmd(" ".join(cmdList), SHORTREADS_ENV)
-        if dryRun:
-            logger.info("Dry run, not executing command")
-            logger.info(cmd)
-            continue
-        res = subprocess.run(
-            cmd, shell=True, capture_output=True, executable=SHELL
+        featureCounts(
+            file=f,
+            gffFile=gffFile,
+            ncpu=ncpu,
+            output_path=output_path,
+            isPe=isPe,
+            targetFeature=targetFeature,
+            groupFactor=groupFactor,
+            fractionCounting=fractionCounting,
+            peLoose=peLoose,
+            dryRun=dryRun,
         )
-        if res.returncode != 0:
-            logger.info(res.stdout.decode())
-            logger.info(res.stderr.decode())
-            raise Exception
-        logger.info(res.stderr.decode())
-        logger.info(f"Finished in {timeDiffStr(ts)}\n")
     logger.info(f"All done, time elapsed {timeDiffStr(finalTs)}")
     logger.info("=" * 20 + getTimeStr() + "=" * 20)
     # Remove logger handler with path "out / align.log"
