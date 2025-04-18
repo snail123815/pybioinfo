0059f4ab57b88adaf41e1b33a3e077504b1bd084
Chao Du <snail123815@users.noreply.github.com>
2025-04-18 15:01:25 +0800

    refactor: update hmmer configuration to use dataclass and streamline parameters

--- pyBioinfo_modules/wrappers/hmmer_config.py
+++ pyBioinfo_modules/wrappers/hmmer_config.py
@@ -1,11 +1,17 @@
-NCPU = 16
+from dataclasses import dataclass
 
-MIN_PROTEIN_LEN = 60
-T_E = 1e-5
-T_INCE = 1e-10
-T_DOME = 1e-5
-T_INCDOME = 1e-10
-GATHER_T_E = 1e-10
-GATHER_T_DOME = 1e-20
-GATHER_T_COV = 0.7
-LEN_DIFF = 0.2  # diff / min(qlen, tlen)
+
+@dataclass
+class HmmerHomologousProtConfig:
+    MIN_PROTEIN_LEN: int = 60
+    T_E: float = 1e-5
+    T_INCE: float = 1e-10
+    T_DOME: float = 1e-5
+    T_INCDOME: float = 1e-10
+    GATHER_T_E: float = 1e-10
+    GATHER_T_DOME: float = 1e-20
+    GATHER_T_COV: float = 0.7
+    LEN_DIFF: float = 0.2  # diff / min(qlen, tlen)
+
+
+hhpc = HmmerHomologousProtConfig()
