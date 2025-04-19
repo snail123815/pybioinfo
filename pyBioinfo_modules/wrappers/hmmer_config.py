76ae02e834955d69b907b01bd43f33be48c15b7b
Chao Du <snail123815@users.noreply.github.com>
2025-04-19 09:26:12 +0800

    Explain the homologous protein configuration

--- pyBioinfo_modules/wrappers/hmmer_config.py
+++ pyBioinfo_modules/wrappers/hmmer_config.py
@@ -4,13 +4,33 @@ from dataclasses import dataclass
 @dataclass
 class HmmerHomologousProtConfig:
     MIN_PROTEIN_LEN: int = 60
-    T_E: float = 1e-5
-    T_INCE: float = 1e-10
-    T_DOME: float = 1e-5
-    T_INCDOME: float = 1e-10
-    GATHER_T_E: float = 1e-10
-    GATHER_T_DOME: float = 1e-20
-    GATHER_T_COV: float = 0.7
+    T_E: float = (
+        1e-5
+        # -E, *report* sequences <= E in output
+    )
+    T_INCE: float = (
+        1e-10
+        # --incE, *significance*, consider seqs <= incE as significance
+    )
+    T_DOME: float = (
+        1e-5
+        # --domE, *report* domains <= domE
+    )
+    T_INCDOME: float = (
+        1e-10
+        # --incdomE, *significance*, consider domains <= incdomE as significance
+    )
+    GATHER_T_E: float = (
+        1e-10
+        # Domtbl filter, on column 7:
+        # E-value of the overall sequence/profile comparison (including all domains)
+    )
+    GATHER_T_DOME: float = (
+        1e-20
+        # Used in cal_cov(), coverage calculation
+        # Only domE <= this value will be considered
+    )
+    GATHER_T_COV: float = 0.7  # Coverage of both query and target
     LEN_DIFF: float = 0.2  # diff / min(qlen, tlen)
 
 
